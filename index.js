d13493d25f0f8dbe8e655d875a5399a7ba2a3ac2
Steven McKeown <stevenmckeown19@gmail.com>
2022-07-07 14:46:25 +0100

    Added function to run server and check file url

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        fs.readFile("index.html", (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            return res.end;
+        });
+    } else if (req.url === "/about") {
+        fs.readFile("about.html", (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            return res.end;
+        });
+    } else if (req.url === "/contact-me") {
+        fs.readFile("contact-me.html", (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            return res.end;
+        });
+    } else {
+        fs.readFile("404.html", (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            return res.end;
+        });
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
