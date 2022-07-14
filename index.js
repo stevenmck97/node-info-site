b3040105bcf2e6c7c952c7c945d6ed53dc0fede4
Steven McKeown <stevenmckeown19@gmail.com>
2022-07-14 12:37:55 +0100

    Refactored to use Express

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const port = process.env.PORT || 3000;
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        fs.readFile("index.html", (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write(data);
-            return res.end;
-        });
-    } else if (req.url === "/about") {
-        fs.readFile("about.html", (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write(data);
-            return res.end;
-        });
-    } else if (req.url === "/contact-me") {
-        fs.readFile("contact-me.html", (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write(data);
-            return res.end;
-        });
-    } else {
-        fs.readFile("404.html", (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write(data);
-            return res.end;
-        });
-    }
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-server.listen(port, () => {
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+    res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.get("/404", (req, res) => {
+    res.sendFile(path.join(__dirname, "/404.html"));
+});
+
+app.listen(port, () => {
     console.log(`Server running at port ${port}`);
 });
